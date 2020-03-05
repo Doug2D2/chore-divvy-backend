6ce2f2ea5fccf6c1ae8d0e1af4ba771445b9aca9
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-05 14:37:20 -0600

    updated test data

--- db/data.sql
+++ db/data.sql
@@ -18,26 +18,24 @@ INSERT INTO frequency (frequency_name) VALUES ('Monthly');
 INSERT INTO frequency (frequency_name) VALUES ('Quarterly');
 INSERT INTO frequency (frequency_name) VALUES ('Yearly');
 
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Wash dishes', 'To do', );
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Laundry', 'To do');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Organize pens', 'In progress');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Cut Grass', 'In progress');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Wash guest room sheets', 'In progress');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Change oil in truck', 'To do');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Make doctor appt', 'To do');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Clean room', 'To do');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Wash dishes', 'To do');
-INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
-VALUES ('Wash dishes', 'To do');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty) 
+VALUES ('Wash dishes', 'To do', 1, 2, 3);
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty) 
+VALUES ('Laundry', 'To do', 2, 4, 1);
+INSERT INTO chores (chore_name, status, category_id, difficulty) 
+VALUES ('Organize pens', 'In progress', 1, 1);
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty) 
+VALUES ('Cut Grass', 'In progress', 3, 4, 5);
+INSERT INTO chores (chore_name, status, category_id, difficulty) 
+VALUES ('Wash guest room sheets', 'In progress', 4, 1);
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty) 
+VALUES ('Change oil in truck', 'Completed', 5, 3, 5);
+INSERT INTO chores (chore_name, status, category_id, difficulty, notes) 
+VALUES ('Make doctor appt', 'To do', 1, 1, 'Derm appt.');
+INSERT INTO chores (chore_name, status, category_id, assignee_id) 
+VALUES ('Clean room', 'To do', 2, 3);
+
+
 
 
 
