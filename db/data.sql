ecfd15461e189821f6e412949e1fed8359927503
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-05 14:36:52 -0600

    switched to postgres, added test data

--- db/data.sql
+++ db/data.sql
@@ -1,10 +1,15 @@
 USE chore_divvy;
 
-INSERT INTO users (user_name) VALUES ('obiONE_kanobi');
-INSERT INTO users (user_name) VALUES ('TWOpac');
-INSERT INTO users (user_name) VALUES ('thrice');
-INSERT INTO users (user_name) VALUES ('quarter_lb');
-INSERT INTO users (user_name) VALUES ('real_houseFIVES_of_nj');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('obiONE_kanobi@email.com', 'password', 'Obione', 'Kanobi');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('TWOpac@email.com', 'password', 'Twopac', 'Shakur');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('thrice@email.com', 'password', 'Thrice', 'Times');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('quarter_lb@email.com', 'password', 'Mickey', 'D');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('real_houseFIVES_of_nj@email.com', 'password', 'Snooki', 'Five');
+
+INSERT INTO categories (category_name, user_id) VALUES ('Personal', '{1}');
+INSERT INTO categories (category_name, user_id) VALUES ('Family Chores', '{1, 2, 3}');
+INSERT INTO categories (category_name, user_id) VALUES ('Mickeys Personal', '{4}');
+INSERT INTO categories (category_name, user_id) VALUES ('Me & hubby', '{4, 5}');
 
 INSERT INTO frequency (frequency_name) VALUES ('Daily');
 INSERT INTO frequency (frequency_name) VALUES ('Weekly');
@@ -12,6 +17,27 @@ INSERT INTO frequency (frequency_name) VALUES ('Bi-Weekly');
 INSERT INTO frequency (frequency_name) VALUES ('Monthly');
 INSERT INTO frequency (frequency_name) VALUES ('Quarterly');
 INSERT INTO frequency (frequency_name) VALUES ('Yearly');
+
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Wash dishes', 'To do', );
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Laundry', 'To do');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Organize pens', 'In progress');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Cut Grass', 'In progress');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Wash guest room sheets', 'In progress');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Change oil in truck', 'To do');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Make doctor appt', 'To do');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Clean room', 'To do');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Wash dishes', 'To do');
+INSERT INTO chores (chore_name, status, frequency_id, category_id, difficulty, notes) 
+VALUES ('Wash dishes', 'To do');
 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,41 +1,58 @@
-CREATE DATABASE IF NOT EXISTS chore_divvy;
-USE chore_divvy;
+-- Database: chore_divvy
 
-CREATE TABLE users (
-    id  INT(10) NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
-);
+-- DROP DATABASE chore_divvy;
 
-CREATE TABLE categories (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    category_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
+CREATE DATABASE chore_divvy
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+	CREATE SEQUENCE user_id_seq;
+	
+	CREATE TABLE users (
+        id  INTEGER DEFAULT NEXTVAL('user_id_seq') NOT NULL,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        PRIMARY KEY(id)
+    );
 
-CREATE TABLE users_categories (
-	user_id INT(10),
-    category_id INT(10),
-    PRIMARY KEY(user_id, category_id)
-);
+	CREATE SEQUENCE categories_id_seq;
 
-CREATE TABLE frequency (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    frequency_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
+    CREATE TABLE categories (
+        id INTEGER DEFAULT NEXTVAL('categories_id_seq') NOT NULL,
+        category_name VARCHAR(30) NOT NULL,
+        user_id INTEGER[],
+        PRIMARY KEY(id)
+    );
 
+    CREATE SEQUENCE freq_id_seq;
 
-CREATE TABLE chores (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    chore_name VARCHAR(255) NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date_completed DATE,
-    frequency_id INT(10) NOT NULL,
-    category_id INT(10) NOT NULL,
-    difficulty INT(1),
-    notes VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (frequency_id) REFERENCES frequency(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
+    CREATE TABLE frequency (
+        id INTEGER DEFAULT NEXTVAL('freq_id_seq') NOT NULL,
+        frequency_name VARCHAR(25) NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+    CREATE SEQUENCE chores_id_seq;
+
+    CREATE TABLE chores (
+        id INTEGER DEFAULT NEXTVAL('chores_id_seq') NOT NULL,
+        chore_name VARCHAR(25) NOT NULL,
+        status VARCHAR(255) NOT NULL,
+        date_completed DATE,
+        frequency_id INTEGER,
+        category_id INTEGER NOT NULL,
+        assignee_id INTEGER,
+        difficulty INTEGER,
+        notes TEXT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (frequency_id) REFERENCES frequency(id),
+        FOREIGN KEY (category_id) REFERENCES categories(id),
+        FOREIGN KEY (assignee_id) REFERENCES users(id)
+    );
