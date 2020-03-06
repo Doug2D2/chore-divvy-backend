de8e7da7f433f031e22e29b16215cf422f149b39
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-05 21:52:16 -0600

    added helmetjs

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(helmet());
 app.use(cors());
 
 app.get('/', function(req, res) {
