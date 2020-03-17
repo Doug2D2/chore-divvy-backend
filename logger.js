365a74d4687fe04a5e9075de09614f292cc5a7f9
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-17 14:29:57 -0500

    started passport setup

---
+++ logger.js
@@ -0,0 +1,26 @@
+const {createLogger, format, transports } = require('winston');
+const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+if(!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, 'results.log');
+
+const logger = createLogger({
+    level: env === 'development' ? 'debug' : 'info',
+    format: format.combine(
+        format.colorize(), 
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+        ),
+    transports: [new transports.File({ filename })]
+});
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
+const logger = require('./logger');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
+app.use(session({ secret: 'cats' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get('/', function(req, res) {
     res.send('Hello World');
@@ -16,4 +24,5 @@ app.get('/', function(req, res) {
 
 app.listen(3000, function() {
     console.log('server running on port 3000');
-})
+});
+
