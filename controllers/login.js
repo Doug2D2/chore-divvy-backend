d086a9b67acbd4380978ce5864fca5fdd121265d
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-26 11:49:10 -0500

    set /login api route

--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 router.post('/login', (req, res) => {
-    res.send('/login working');
+    const usernameInput = req.body.username;
+    const passwordInput = req.body.password;
+
+    console.log(db.user);
+    db.user.findAll({
+        where: {
+            username: usernameInput,
+            password: passwordInput
+        }
+    })
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.json(err));
+    
+    //res.send(`username: ${usernameInput} password: ${passwordInput}`);
 });
 
 router.post('/sign-up', (req, res) => {
