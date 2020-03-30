ba1a3934f5f363f001be1cffc3e52d6632e9a1d2
Matt Vella <mattvella07@gmail.com>
2020-03-29 23:08:40 -0500

    Working on terraform to create server and db

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,11 +31,11 @@ resource "aws_db_instance" "chore_divvy_db" {
 }
 
 resource "aws_instance" "chore_divvy_server" {
-  ami               = var.ami
-  instance_type     = "t2.micro"
+    ami               = var.ami
+    instance_type     = "t2.micro"
 
-  root_block_device {
-      volume_type = "gp2"
-      volume_size = 10 
-  }
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = 10 
+    }
 }
