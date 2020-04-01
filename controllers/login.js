60155dac0efc8b3ef5dc596f61cca30549adc569
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-01 15:05:20 -0500

    sign-up route

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const logger = require('../logger');
 
 router.post('/login', (req, res) => {
     const usernameInput = req.body.username;
@@ -20,11 +21,32 @@ router.post('/login', (req, res) => {
         res.status(200);
         return res.json(data[0]);
     })
-    .catch(err => { res.sendStatus(500) });
+    .catch(err => { 
+        res.sendStatus(500) 
+        logger.error(err);
+    });
 });
 
 router.post('/sign-up', (req, res) => {
-    res.send('/sign-up working');
+    const username = req.body.username;
+    const password = req.body.password;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+
+    db.user.create({
+        username: username,
+        password: password,
+        first_name: firstName,
+        last_name: lastName
+    })
+    .then(data => {
+        res.status(200);
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(500);
+        logger.error(err);
+    });
 });
 
 module.exports = router;
