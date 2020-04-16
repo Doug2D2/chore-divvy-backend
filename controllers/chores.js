1c0e56ca4448843cf519c17b58dd8f67294332a3
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-16 14:18:28 -0500

    /get-chores-by-id route

--- controllers/chores.js
+++ controllers/chores.js
@@ -4,22 +4,35 @@ const db = require('../models');
 const logger = require('../logger');
 
 router.get('/get-chores', (req, res) => {
-    res.send('/get-chores working');
-
     db.chore.findAll()
     .then(data => {
-        console.log(data);
+        res.status(200);
+        return res.json(data);
     })
     .catch(err => {
         logger.error(err);
         res.status(500);
         return res.json({ errMessage: 'Server Error' });
     })
-
 });
 
 router.get('/get-chore-by-id', (req, res) => {
-    res.send('/get-chore-by-id working');
+    const choreId = req.body.id;
+
+    db.chore.findAll({
+        where: {
+            id: choreId
+        }
+    })
+    .then(data => {
+        res.status(200);
+        return res.json(data);
+    })
+    .catch(err => {
+        logger.error(err);
+        res.status(500);
+        return res.json({ errMessage: 'Server Error' });
+    })
 });
 
 router.post('/add-chore', (req, res) => {
