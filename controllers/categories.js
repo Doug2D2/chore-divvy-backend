25e364b7d9d36e9df88fd9ac0b7c2919b7beaf55
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-21 15:00:08 -0500

    /get-categories route completed & tested

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const logger = require('../logger');
 
 router.get('/get-categories', (req, res) => {
-    res.send('/get-categories working');
+    db.category.findAll()
+    .then(data => {
+        res.status(200);
+        return res.json(data);
+    })
+    .catch(err => {
+        logger.error(err);
+        res.status(500);
+        return res.json({ errMessage: 'Server Error' });
+    });
 });
 
-router.get('/get-categories-by-id', (req, res) => {
-    res.send('/get-categories-by-id working');
+router.get('/get-categories/:id', (req, res) => {
+    const id = req.params.id;
+    
 });
 
 router.post('/add-category', (req, res) => {
-    res.send('/add-category working');
+    
 });
 
-router.put('/update-category', (req, res) => {
-    res.send('/update-category working');
+router.put('/update-category/:id', (req, res) => {
+    const id = req.params.id;
+    
 });
 
-router.delete('/delete-category', (req, res) => {
-    res.send('/delete-cateogry working');
+router.delete('/delete-category/:id', (req, res) => {
+    const id = req.params.id;
+    
 });
 
 module.exports = router;
