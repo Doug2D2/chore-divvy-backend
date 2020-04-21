f660b5a937f6cb70f17f3ada5b6ccd71c15cc34e
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-21 15:31:33 -0500

    /update-category/:id completed & tested

--- controllers/categories.js
+++ controllers/categories.js
@@ -58,9 +58,28 @@ router.post('/add-category', (req, res) => {
 
 router.put('/update-category/:id', (req, res) => {
     const id = req.params.id;
+    const { 
+        categoryName,
+        userIds
+     } = req.body;
 
-
-    
+    db.category.update({
+        category_name: categoryName,
+        user_id: userIds
+    }, {
+        where: {
+            id: id
+        }
+    })
+    .then(data => {
+        res.status(200);
+        return res.json(data);
+    })
+    .catch(err => {
+        logger.error(err);
+        res.status(500);
+        return res.json({ errMessage: 'Server Error' });
+    });
 });
 
 router.delete('/delete-category/:id', (req, res) => {
