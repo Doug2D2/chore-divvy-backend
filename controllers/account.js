5779a83d8cec1880ea6aec73831df39ab6bbe84c
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-27 12:04:09 -0500

    completed & tested get all users route

--- controllers/account.js
+++ controllers/account.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const logger = require('../logger');
+
+router.get('/get-users', (req, res) => {
+    db.user.findAll()
+    .then(data => {
+        res.status(200);
+        return res.json(data);
+    })
+    .catch(err => {
+        logger.error(err);
+        res.status(500);
+        return res.json({ errMessage: 'Server Error' });
+    });
+});
 
 router.put('/update-account/:id', (req, res) => {
     res.send('/update-account working');
