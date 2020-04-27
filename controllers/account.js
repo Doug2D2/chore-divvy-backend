b567a2b6d7ba4488752edbed2c92788508c2520f
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-27 14:24:21 -0500

    added bcrypt, tested.

--- controllers/account.js
+++ controllers/account.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const logger = require('../logger');
+const bcrypt = require('bcrypt');
 
 router.get('/get-users', (req, res) => {
     db.user.findAll()
@@ -36,7 +37,6 @@ router.get('/get-user-by-id/:id', (req, res) => {
 });
 
 router.put('/update-account/:id', (req, res) => {
-
     const id = req.params.id;
     const {
         password,
@@ -44,25 +44,34 @@ router.put('/update-account/:id', (req, res) => {
         lastName
     } = req.body;
 
+
     if(password.length >= 8) {
-        db.user.update({
-            password: password,
-            first_name: firstName,
-            last_name: lastName
-        }, {
-            where: {
-                id: id
+        bcrypt.hash(password, 10, function(err, hash) {
+            if(err) {
+                logger.error(err);
+                res.status(500);
+                return res.json({ errMessage: 'Server Error' });
             }
+
+            db.user.update({
+                password: hash,
+                first_name: firstName,
+                last_name: lastName
+            }, {
+                where: {
+                    id: id
+                }
+            })
+            .then(data => {
+                res.status(200);
+                return res.json(data);
+            })
+            .catch(err => {
+                logger.error(err);
+                res.status(500);
+                return res.json({ errMessage: 'Server Error' });
+            });
         })
-        .then(data => {
-            res.status(200);
-            return res.json(data);
-        })
-        .catch(err => {
-            logger.error(err);
-            res.status(500);
-            return res.json({ errMessage: 'Server Error' });
-        });
     } else {
         res.status(400);
         return res.json({ errMessage: 'Password must be at least 8 characters' });
