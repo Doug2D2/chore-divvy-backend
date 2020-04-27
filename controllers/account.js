697ab37d75ae6c09e635fa350c97fe1ab9bece10
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-27 11:45:50 -0500

    completed update acct-route

--- controllers/account.js
+++ controllers/account.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-router.put('/update-account', (req, res) => {
+router.put('/update-account/:id', (req, res) => {
     res.send('/update-account working');
+    const id = req.params.id;
+    const {
+        username,
+        password,
+        firstName,
+        lastName
+    } = req.body;
+
+    db.user.update({
+        username: username,
+        password: password,
+        first_name: firstName,
+        last_name: lastName
+    }, {
+        where: {
+            id: id
+        }
+    })
+    .then(data => {
+        res.status(200);
+        return res.json(data);
+    })
+    .catch(err => {
+        logger.error(err);
+        res.status(500);
+        return res.json({ errMessage: 'Server Error' });
+    });
 });
 
 module.exports = router;
