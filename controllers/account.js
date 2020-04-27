b7f46f761c3d6ddddb020d45a0d9210bc8ffa41d
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-27 14:16:39 -0500

    checking for password length

--- controllers/account.js
+++ controllers/account.js
@@ -36,34 +36,37 @@ router.get('/get-user-by-id/:id', (req, res) => {
 });
 
 router.put('/update-account/:id', (req, res) => {
-    res.send('/update-account working');
+
     const id = req.params.id;
     const {
-        username,
         password,
         firstName,
         lastName
     } = req.body;
 
-    db.user.update({
-        username: username,
-        password: password,
-        first_name: firstName,
-        last_name: lastName
-    }, {
-        where: {
-            id: id
-        }
-    })
-    .then(data => {
-        res.status(200);
-        return res.json(data);
-    })
-    .catch(err => {
-        logger.error(err);
-        res.status(500);
-        return res.json({ errMessage: 'Server Error' });
-    });
+    if(password.length >= 8) {
+        db.user.update({
+            password: password,
+            first_name: firstName,
+            last_name: lastName
+        }, {
+            where: {
+                id: id
+            }
+        })
+        .then(data => {
+            res.status(200);
+            return res.json(data);
+        })
+        .catch(err => {
+            logger.error(err);
+            res.status(500);
+            return res.json({ errMessage: 'Server Error' });
+        });
+    } else {
+        res.status(400);
+        return res.json({ errMessage: 'Password must be at least 8 characters' });
+    }
 });
 
 module.exports = router;
