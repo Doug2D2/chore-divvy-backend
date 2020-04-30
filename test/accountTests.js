77f0fb776027c6008a99d0605f8fad34adc118a3
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-30 15:52:44 -0500

    install mocha chai & chai-http, start writing tests

---
+++ test/accountTests.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chair.expect;
+const db = require('../models');
+
+chai.should();
+
+describe('Account Tests', function() {
+    describe('/get-users', function() {
+        it('should return all users in user table', function() {
+
+        });
+    });
+
+    // describe('', function() {
+
+    // });
+
+    // describe('', function() {
+
+    // });
+});
