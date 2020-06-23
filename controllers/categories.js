8565716bf155fbad7369f14f356de8e4cd52740c
Natalie Vasquez <natalievasquez11@gmail.com>
2020-06-23 17:12:55 -0500

    added /get-categories-by-userId/:userId

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,9 +2,31 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const logger = require('../logger');
+const Sequelize = require('sequelize');
 
 router.get('/get-categories', (req, res) => {
     db.category.findAll()
+    .then(data => {
+        res.status(200);
+        return res.json(data);
+    })
+    .catch(err => {
+        logger.error(err);
+        res.status(500);
+        return res.json({ errMessage: 'Server Error' });
+    });
+});
+
+router.get('/get-categories-by-userId/:userId', (req, res) => {
+    const userId = req.params.userId;
+
+    db.category.findAll({
+        where: {
+            user_id: {
+                [Sequelize.Op.contains]: [userId]
+            }
+        }
+    })
     .then(data => {
         res.status(200);
         return res.json(data);
