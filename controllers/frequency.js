953ca20753eea818ba44b5e16dbc8f77d9b7a352
Natalie Vasquez <natalievasquez11@gmail.com>
2020-11-06 11:46:59 -0600

    Removed console.logs and added message to logger error frequency.js

--- controllers/frequency.js
+++ controllers/frequency.js
@@ -6,13 +6,11 @@ const logger = require('../logger');
 router.get('/get-frequencies', (req, res) => {
     db.frequency.findAll()
     .then(data => {
-        console.log('is this thing working?');
         res.status(200);
         return res.json(data);
     })
     .catch(err => {
-        console.log('this thing isnt working.');
-        logger.error(err);
+        logger.error('GET /get-frequencies', err);
         res.status(500);
         return res.json({ errMessage: 'Server Error'});
     });
